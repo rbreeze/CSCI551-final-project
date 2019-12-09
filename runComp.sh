2bec6121fa7ed6612e79b56b22c1ba0c9713a2d2
Remington Breeze <remington.breeze@gmail.com>
2019-12-09 00:42:25 -0800

    Updated script

--- runComp.sh
+++ runComp.sh
@@ -1,4 +1,4 @@
-printf "python,"
+printf "python,$($2),"
 mpirun -f hosts python3 $1/$1.py $2 | grep "Elapsed" | awk '{print $2}' >> results/$1.csv
-printf "c,"
+printf "c,,$($2),"
 mpirun -f hosts ./$1/$1 $2 | grep "Elapsed" | awk '{print $2}' >> results/$1.csv
