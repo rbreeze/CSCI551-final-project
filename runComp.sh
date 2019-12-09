e853c0c324fe9f30a06d15cd6e812f7d6b5dc3b1
Remington Breeze <remington.breeze@gmail.com>
2019-12-09 00:52:17 -0800

    Tweaked run scripts

--- runComp.sh
+++ runComp.sh
@@ -1,4 +1,6 @@
 printf "python,$2," >> results/$1.csv
+echo "Testing $1.py with $2 cycles"
 mpirun -f hosts python3 $1/$1.py $2 | grep "Elapsed" | awk '{print $3}' >> results/$1.csv
 printf "c,$2," >> results/$1.csv
+echo "Testing $1.c with $2 cycles"
 mpirun -f hosts ./$1/$1 $2 | grep "Elapsed" | awk '{print $3}' >> results/$1.csv
