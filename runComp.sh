a45c070c9ed7c0e43513655be3f6754625cd0885
Remington Breeze <remington.breeze@gmail.com>
2019-12-09 00:39:17 -0800

    Updated running script

--- runComp.sh
+++ runComp.sh
@@ -1,2 +1,4 @@
-mpirun -f hosts python3 $1/$1.py $2
-mpirun -f hosts ./$1/$1 $2
+printf "python,"
+mpirun -f hosts python3 $1/$1.py $2 | grep "Elapsed" | awk '{print $2}' >> results/$1.csv
+printf "c,"
+mpirun -f hosts ./$1/$1 $2 | grep "Elapsed" | awk '{print $2}' >> results/$1.csv
