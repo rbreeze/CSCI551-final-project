ef69ea2810b2f52c37efe5ac37b0f871f6e27608
Remington Breeze <remington.breeze@gmail.com>
2019-12-08 22:10:13 -0800

    Went back to my old port of the primes.c program to python

--- primes/primes2.py
+++ primes/primes2.py
@@ -1,163 +1,52 @@
-#! /usr/bin/env python
-#
-def prime_mpi ( ):
+# adapted from Thomas Weise
+# https://github.com/thomasWeise/distributedComputingExamples/blob/master/mpi/reducePrimes.c
 
-#*****************************************************************************80
-#
-## PRIME_MPI counts the primes between N_LO and N_HI.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    01 October 2016
-#
-#  Author:
-#
-#    John Burkardt
-#
-  import numpy as np
-  import platform
-  import sys
-  from mpi4py import MPI
+from mpi4py import MPI
+import sys
+import numpy
+import math
+from time import process_time 
 
-  comm = MPI.COMM_WORLD
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
 
-  id = comm.Get_rank ( )
+if size<2:
+  print("Need more than 1 process")
+  quit()
 
-  p = comm.Get_size ( )
+DATA_SIZE = 2048
 
-  n_lo = 1
-  n_hi = 131072
-  n_factor = 2
+send = numpy.zeros(DATA_SIZE, dtype=int)
+recv = numpy.zeros(DATA_SIZE, dtype=int)
 
-  if id == 0:
-    wtime = MPI.Wtime ( )
-    print ( '' )
-    print ( 'PRIME_MPI' )
-    print ( '  Python version: %s' % ( platform.python_version ( ) ) )
-    print ( '  Count the primes between %d and %d' % ( n_lo, n_hi )
-    print ( '' )
-    print ( '  Use MPI to divide the computation among' )
-    print ( '  multiple processes.' )
-#
-#  Search the range [2,N] for  primes.
-#  After each search, double N and do it again.
-#
-  n = n_lo
+t1_start = process_time() 
 
-  while n <= n_hi:
-#
-#  The PRIMES array starts out as an empty list of integers.
-#
-    primes = np.array ( 0, dtype = np.int32 )
-    wtime = MPI.Wtime ( )
-#
-#  T counts the primes that process ID finds.
-#
-    t = 0
-#
-#  Process 0 checks   2+0,   2+0  +P, 2+0  +2P  ...
-#  Process 1 checks   2+1,   2+1  +P, 2+1  +2P, ...
-#  Process P-1 checks 2+P-1, 2+P-1+P, 2+P-1+2P, ... 
-#
-    for i in range ( 2 + id, n + 1, p ):
-      isprime = 1
-      for j in range ( 2, i ):
-        if ( i % j ) == 0:
-          isprime = 0
-          break
-      t = t + isprime
+if (rank == 0): 
+  for i in range(DATA_SIZE, 0, -1): 
+    send[i] = (i + 1)
 
-    comm.Reduce ( [ t, MPI.DOUBLE ], [ primes, MPI.INT ], op = MPI.SUM, root = 0 )
+  comm.scatter(send, recv, root=0)
 
-    wtime = MPI.Wtime ( ) - wtime
+count = int(DATA_SIZE / size)
 
-    if id == 0:
-      print ( '  %d  %d  %g', % ( n, primes, wtime ) )
+res = count
 
-    n = n * n_factor
-#
-#  Terminate.
-#
-  if id == 0:
-    print ( '' )
-    print ( 'PRIME_MPI:' )
-    print ( '  Normal end of execution.' )
+for i in range(count, 0, -1):  
+  for j in range(int(math.sqrt(recv[i]))|1, 1, -2): 
+    if ((recv[i] % j) == 0):
+      res -= 1
+      break 
 
-  return
+t1_stop = process_time() 
 
-def timestamp ( ):
+print("Process " + str(rank) + " discovered " + str(res) + " primes in the numbers from " + str(recv[0]) + " to " + str(recv[count-1]) + ".")
 
-#*****************************************************************************80
-#
-## TIMESTAMP prints the date as a timestamp.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license. 
-#
-#  Modified:
-#
-#    06 April 2013
-#
-#  Author:
-#
-#    John Burkardt
-#
-#  Parameters:
-#
-#    None
-#
-  import time
+res = numpy.sum(numpy.array(res)).astype('float64')
+recv = numpy.sum(numpy.array(recv)).astype('float64')
 
-  t = time.time ( )
-  print ( time.ctime ( t ) )
+comm.Reduce(res, recv, op=MPI.SUM, root=0)
 
-  return None
-
-def timestamp_test ( ):
-
-#*****************************************************************************80
-#
-## TIMESTAMP_TEST tests TIMESTAMP.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license. 
-#
-#  Modified:
-#
-#    03 December 2014
-#
-#  Author:
-#
-#    John Burkardt
-#
-#  Parameters:
-#
-#    None
-#
-  import platform
-
-  print ( '' )
-  print ( 'TIMESTAMP_TEST:' )
-  print ( '  Python version: %s' % ( platform.python_version ( ) ) )
-  print ( '  TIMESTAMP prints a timestamp of the current date and time.' )
-  print ( '' )
-
-  timestamp ( )
-#
-#  Terminate.
-#
-  print ( '' )
-  print ( 'TIMESTAMP_TEST:' )
-  print ( '  Normal end of execution.' )
-  return
-
-if ( __name__ == '__main__' ):
-  timestamp ( )
-  prime_mpi ( )
-  timestamp ( )
+if(rank == 0): 
+  print("The total number of primes in the first " + str(count*size) + " natural numbers is " + str(recv[0]) + ".")
+  print("Time: " + str(t1_stop-t1_start))
